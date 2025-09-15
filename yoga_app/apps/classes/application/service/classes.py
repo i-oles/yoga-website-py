af36657b369465b96933dc2d0fb739b8ae46d84a
Igor Oles <igor.oles@websensa.com>
2025-09-16 01:38:27 +0200

    repo added to workflow

--- yoga_app/apps/classes/application/service/classes.py
+++ yoga_app/apps/classes/application/service/classes.py
@@ -1,13 +1,13 @@
-import time
 import uuid
 
 from ...domain.entities import YogaClassParams, YogaClass
+from ...domain.repositories.repositories import IClassesRepository
 from ...domain.services.services import IClassesService
 
 
 class ClassesService(IClassesService):
-    def __init__(self):
-        # self.classes_repo = classes_repo
+    def __init__(self, classes_repo: IClassesRepository):
+        self.classes_repo = classes_repo
         pass
 
     def create_classes(self, yoga_class_params: list[YogaClassParams]) -> list[YogaClass]:
@@ -23,6 +23,6 @@ class ClassesService(IClassesService):
             ) for p in yoga_class_params]
 
         # TODO: handle error here
-        # inserted_classes = self.classes_repo.insert(classes)
+        inserted_classes = self.classes_repo.insert(classes)
 
-        return classes
+        return inserted_classes
--- yoga_app/apps/classes/interfaces/api/urls.py
+++ yoga_app/apps/classes/interfaces/api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views import classes_view
 from ...application.service.classes import ClassesService
-
-# classes_repo = ClassesRepo()
+from ...infrastructure.sqlite.classes import ClassesRepo
 
 urlpatterns = [
-    path("", classes_view(ClassesService()))
+    path("", classes_view(ClassesService(ClassesRepo())))
 ]
