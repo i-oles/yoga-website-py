0bb0af412a7f0f8b6ad14a8e960dda2c7ff9ecae
Igor Oles <igor.oles@websensa.com>
2025-09-17 11:59:20 +0200

    wip test

--- yoga_app/classes/tests/test_views.py
+++ yoga_app/classes/tests/test_views.py
@@ -2,11 +2,11 @@ import datetime
 import uuid
 import pytest
 from datetime import timedelta
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from django.test import Client
 from django.urls import path
-from yoga_app.apps.classes.domain.entities import YogaClass
-from yoga_app.apps.classes.interfaces.api.views import classes_view
+from yoga_app.classes.domain.entities import YogaClass
+from yoga_app.classes.interfaces.api.views import classes_view
 
 test_class_id = uuid.uuid4()
 test_future_date = datetime.datetime.now() + timedelta(2)
@@ -33,7 +33,7 @@ def mock_yoga_class() -> YogaClass:
 def yoga_class_response():
     return {
         "id": f"{test_class_id}",
-        "start_time": f"{test_future_date}",
+        "start_time": test_future_date.isoformat(),
         "class_name": "vinyasa",
         "class_level": "intermediate",
         "max_capacity": 10,
@@ -42,18 +42,15 @@ def yoga_class_response():
 
 
 # ------------- Test POST api/v1/classes ---------------
-@pytest.mark.django
-def test_create_classes_success(mock_yoga_class, yoga_class_response):
-    mock_service = MagicMock()
-    mock_service.create_classess.return_value = [mock_yoga_class]
-
-    _ = classes_view(mock_service)
+@patch("yoga_app.classes.interfaces.api.views.ClassesService")
+@pytest.mark.django_db
+def test_create_classes_success(mock_service, mock_yoga_class, yoga_class_response):
+    mock_service.return_value.create_classes.return_value = [mock_yoga_class]
 
     client = Client()
-
     body = [
         {
-            "start_time": f"{test_future_date}",
+            "start_time": "2025-09-20T10:00:00Z",
             "class_level": "intermediate",
             "class_name": "vinyasa",
             "max_capacity": 10,
@@ -61,8 +58,9 @@ def test_create_classes_success(mock_yoga_class, yoga_class_response):
         }
     ]
 
-    response = client.post("/classes/", data=body, content_type="application/json")
+    response = client.post("/", data=body, content_type="application/json")
+
     assert response.status_code == 200
-    data = response.body
-    assert "classes" in data
+    data = response.json()
     assert data["classes"][0] == yoga_class_response
+
